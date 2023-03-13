d530620e18400d7e792eb6c520f1fdd260649d60
nikki <127743263+bajentin@users.noreply.github.com>
2023-03-13 18:07:41 +0700

    Create Order.py

---
+++ Order.py
@@ -0,0 +1,18 @@
+Date = ''
+Status = ''
+ShoppingCart = ''
+DeliveryAddress = ''
+PaymentMethod = ''
+
+def validatePayment():
+    return()
+def cancel():
+    return()
+def dispatch():
+    return()
+def confirmDelivery():
+    return()
+def refund():
+    return()
+def archive():
+    return()
