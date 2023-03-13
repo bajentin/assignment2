ab049e479af7f1dda9f281fe3a2fedcd67a46834
nikki <127743263+bajentin@users.noreply.github.com>
2023-03-13 18:09:14 +0700

    Create ShoppingCart.py

---
+++ ShoppingCart.py
@@ -0,0 +1,17 @@
+ListOfItems = ''
+DeliveryFees = ''
+Discount = ''
+TotalAmount = ''
+
+def addItem():
+    return()
+def removeItem():
+    return()
+def selectDeliveryOption():
+    return()
+def applyDiscount():
+    return()
+def calculateTotal():
+    return()
+def checkout():
+    return()
