eb635b6082d6a2048d65481689867771c766d66d
nikki <127743263+bajentin@users.noreply.github.com>
2023-03-13 18:06:46 +0700

    Create MP3.py

---
+++ MP3.py
@@ -0,0 +1,18 @@
+class Item:
+
+    Code = ''
+    Title = ''
+    Description1 = ''
+    Category = ''
+    Picture = ''
+    Description2 = ''
+    QuantityInShock = ''
+    Price = ''
+
+def viewFullDescription():
+    return()
+def addToCart():
+    return()
+def updateStockLevel():
+    return()
+
